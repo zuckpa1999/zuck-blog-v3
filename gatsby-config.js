1372fa8a0a5b0f9597afd6c79b9a0f77e21fe255
zuck_pa <zuckpa1999@gmail.com>
2025-04-19 19:58:46 +0700

    Revert "fix: add pre-fetch styled component to fix unapplied css during prefetch"
    This reverts commit 897743592f9f8af342098ed685d876668c62c10b.

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,6 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-          `gatsby-plugin-styled-components`,
         ],
       },
     },
